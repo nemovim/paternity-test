ccc8e1e88397af88061dc9924bf1992a247663a4
nemovim <nemovim.dev.gmail.com>
2024-06-13 05:05:58 +0900

    feat: Save preprocessed images

---
+++ dataset_to_pt.py
@@ -0,0 +1,25 @@
+import torch
+from torchvision import transforms
+from PIL import Image
+import os, glob
+
+trans = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    transforms.Resize([224, 224])
+])
+
+path = './dataset/test/families'
+# image_paths = [*glob.glob(os.path.join(path, "*.JPG")), *glob.glob(os.path.join(path, "*.jpg"))]
+image_paths = glob.glob(os.path.join(path, "*.JPG"))
+print(len(image_paths))
+
+for image_path in image_paths:
+  print(image_path)
+  image = Image.open(image_path)
+  image = trans(image)
+
+  path_arr = image_path.split(os.path.sep)
+  fileName = path_arr[-1]
+  fileName = '.'.join([*fileName.split('.')[:-1], 'pt'])
+  torch.save(image, os.path.join((os.path.sep).join(path_arr[:-1]), fileName))
